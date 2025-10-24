5de64329c87f88b29c9ef1ce1b7de78c520227b2
ABDUL MOIZ <babamoiz289@gmail.com>
2025-10-24 17:52:41 +0500

    completed middleware auth

---
+++ ai-ticket-asistant/middlewares/auth.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const authenticate = (req, res, next) => {
+  const token = req.headers.authorizations?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Authentication token missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid authentication token" });
+  }
+};
