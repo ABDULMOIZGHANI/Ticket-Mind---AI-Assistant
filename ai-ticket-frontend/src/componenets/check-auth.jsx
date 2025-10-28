3cc10fe8aa766965a05b62d530053f8fee7bc412
ABDUL MOIZ <babamoiz289@gmail.com>
2025-10-28 19:45:33 +0500

    chcking authentication code

--- ai-ticket-frontend/src/componenets/check-auth.jsx
+++ ai-ticket-frontend/src/componenets/check-auth.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const CheckAuth = () => {
-    return (
-        <div>
+function CheckAuth({ children, protectedRoute }) {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
 
-        </div>
-    )
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (protectedRoute) {
+            if (!token) {
+                navigate("/login");
+            } else {
+                setLoading(false);
+            }
+        } else {
+            if (token) {
+                navigate("/");
+            } else {
+                setLoading(false);
+            }
+        }
+    }, [navigate, protectedRoute]);
+
+    if (loading) {
+        return <div>loading...</div>;
+    }
+    return children;
 }
 
-export default CheckAuth
+export default CheckAuth;
